d5d9dd2f29efbfdba934913284efc0155f2e037e
Hank <45217365+hsmoon5458@users.noreply.github.com>
2021-10-15 15:58:32 -0400

    OVRSkeleton Updated

--- VRHapticGloveV4/Assets/Scripts/NetworkPlayer.cs
+++ VRHapticGloveV4/Assets/Scripts/NetworkPlayer.cs
@@ -10,36 +10,51 @@ public class NetworkPlayer : MonoBehaviour
     public Transform networkPlayerLeftHand;
     public Transform networkPlayerRightHand;
 
-    
     private PhotonView photonView;
 
+    //head and hand position settings
     private Transform myHeadRig;
     private Transform myLeftHandRig;
     private Transform myRightHandRig;
 
+    //fingers setting
+    private GameObject leftHandPrefab, rightHandPrefab; // to assign OVRskeleton in Network player, need access parent to GetComponent
+    private OVRSkeleton leftHandSkeleton, rightHandSkeleton;
+    private List<OVRBone> leftFingerBones, rightFingerBones;
+    public List<Transform> networkPlayerLeftFingerBones, networkPlayerRightFingerBones;
     void Start()
     {
         photonView = GetComponent<PhotonView>();
-        
+        StartCoroutine(BoneIdentify()); // wait few second for the systems to recognize the bones properly at the beginning
 
+        leftHandPrefab = GameObject.Find("HankOVRCameraRig/TrackingSpace/LeftHandAnchor/OVRHandPrefab");
+        rightHandPrefab = GameObject.Find("HankOVRCameraRig/TrackingSpace/RightHandAnchor/OVRHandPrefab");
+        Debug.Log(leftHandPrefab);
+        Debug.Log(rightHandPrefab);
         try
         {
-            OVRCameraRig OVRrig = FindObjectOfType<OVRCameraRig>();
+            OVRCameraRig OVRrig = FindObjectOfType<OVRCameraRig>(); //get OVRrig
 
             myHeadRig = OVRrig.transform.Find("TrackingSpace/CenterEyeAnchor");
             myLeftHandRig = OVRrig.transform.Find("TrackingSpace/LeftHandAnchor/OVRHandPrefab");
             myRightHandRig = OVRrig.transform.Find("TrackingSpace/RightHandAnchor/OVRHandPrefab");
 
+            leftHandSkeleton = leftHandPrefab.GetComponent<OVRSkeleton>();
+            rightHandSkeleton = rightHandPrefab.GetComponent<OVRSkeleton>();
+            Debug.Log(leftHandSkeleton);
+            Debug.Log(rightHandSkeleton);
         }
         catch
         {
             Debug.LogError("OVR not found");
-        }      
+        }
+
     }
    
     // Update is called once per frame
     void Update()
     {
+
         if (photonView.IsMine)
         {
             networkPlayerHead.gameObject.SetActive(false);
@@ -57,5 +72,13 @@ public class NetworkPlayer : MonoBehaviour
     {
         networkPlayer.position = myTransform.position;
         networkPlayer.rotation = myTransform.rotation;
+    }
+
+    //delay at start for the system to identify the bones at the beginning
+    private IEnumerator BoneIdentify()
+    {
+        yield return new WaitForSeconds(2f);
+        leftFingerBones = new List<OVRBone>(leftHandSkeleton.Bones);
+        rightFingerBones = new List<OVRBone>(rightHandSkeleton.Bones);
     }
 }
