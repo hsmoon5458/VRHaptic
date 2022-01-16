953cf0347112a586385536dc4a4254b8d2408778
Hank <45217365+hsmoon5458@users.noreply.github.com>
2022-01-15 21:21:48 -0500

    Sample guides upated

--- VRHapticGloveV4/Assets/Scripts/NetworkObjectAxisSetup.cs
+++ VRHapticGloveV4/Assets/Scripts/NetworkObjectAxisSetup.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class NetworkObjectAxisSetup : MonoBehaviour
 {
     public GameObject xAxis, yAxis, zAxis;
-    
+    public float FixeScale = 1;
+    public GameObject parent;
+
     void Update()
     {
-        xAxis.transform.localScale = new Vector3(0.1f, 1, 0.1f);
-        yAxis.transform.localScale = new Vector3(0.1f, 1, 0.1f);
-        zAxis.transform.localScale = new Vector3(0.1f, 1, 0.1f);
+        xAxis.transform.localScale = new Vector3(FixeScale / parent.transform.localScale.x * 0.1f, FixeScale / parent.transform.localScale.y, FixeScale / parent.transform.localScale.z * 0.1f);
+        yAxis.transform.localScale = new Vector3(FixeScale / parent.transform.localScale.x * 0.1f, FixeScale / parent.transform.localScale.y, FixeScale / parent.transform.localScale.z * 0.1f);
+        zAxis.transform.localScale = new Vector3(FixeScale / parent.transform.localScale.x * 0.1f, FixeScale / parent.transform.localScale.y, FixeScale / parent.transform.localScale.z * 0.1f);
     }
 }
